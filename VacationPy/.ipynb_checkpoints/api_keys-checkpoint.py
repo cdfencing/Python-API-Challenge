b64d5d8f79cd76acd5a5cac357d45a9e19a8f3b2
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-07-14 18:28:23 -0500

    Python-APIs Challenge

--- VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,4 @@
-# OpenWeatherMap API Key
-weather_api_key = "8c6078fe"
+weather_api_key = "9d88a4e32284086ca01a89386c890bf8"
 
 # Google API Key
-g_key = "8c6078fe"
+g_key = "AIzaSyDIIsbPPUE7wVmen-wmwrYAXB1L4WQif1U"
